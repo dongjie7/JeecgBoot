2a1103c52abfd10bc459e6dc01192bd25cfa3e51
0638 <978692700@qq.com>
2024-06-30 23:13:40 +0800

    提交代码--合并最新的代码20240630_4

--- jeecg-boot-base-core/src/main/java/org/jeecg/config/RestTemplateConfig.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package org.jeecg.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -16,10 +17,11 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(@Qualifier("clientHttpRequestFactory") ClientHttpRequestFactory factory) {
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
         return new RestTemplate(factory);
     }
 
+    @Primary
     @Bean
     public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
